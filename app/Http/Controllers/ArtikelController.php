27974df09d3892209acf1946b50fb39d1bab7b14
GRAVVLIN <pradana.arifan20@gmail.com>
2024-09-02 19:40:59 +0700

    routing/jalur untuk artikel

--- app/Http/Controllers/ArtikelController.php
+++ app/Http/Controllers/ArtikelController.php
@@ -13,7 +13,7 @@ class ArtikelController extends Controller
      */
     public function index()
     {
-        return view();
+        return view('page.artikel');
     }
 
     /**
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
+use App\Http\Controllers\ArtikelController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/artikel',[ArtikelController::class,'index'])->name('halamanArtikel');
+Route::get('/artikelCari/{id}',[ArtikelController::class])->name('cariArtikel');
+Route::put('/artikelEdit/{id}',[ArtikelController::class,'update'])->name('editArtikel');
+Route::delete('/artikelHapus/{id}',[ArtikelController::class,'destroy'])->name('deleteArtikel');
