201b82b0df51728f97318bd9d73fcdb99d8df676
fathursyh <fathur.syah1@gmail.com>
2024-09-15 19:17:19 +0700

    minor update : title update

--- resources/views/layouts/auth-layout.blade.php
+++ resources/views/layouts/auth-layout.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Comstract - Login</title>
+    <title>Comstract</title>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body>
