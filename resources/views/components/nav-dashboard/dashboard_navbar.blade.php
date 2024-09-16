c31e0520f510f25f0d4d1b0863a44cfca4853c5a
fathursyh <fathur.syah1@gmail.com>
2024-09-16 19:26:51 +0700

    update : sidebar responsivity

--- resources/views/components/nav-dashboard/dashboard_navbar.blade.php
+++ resources/views/components/nav-dashboard/dashboard_navbar.blade.php
@@ -1,12 +1,12 @@
 
-<button data-drawer-target="logo-sidebar" data-drawer-toggle="logo-sidebar" aria-controls="logo-sidebar" type="button" class="inline-flex items-center p-2 mt-2 ms-3 text-sm text-gray-500 rounded-lg sm:hidden focus:outline-none focus:ring-2 focus:ring-gray-200">
+<button data-drawer-target="logo-sidebar" data-drawer-toggle="logo-sidebar" aria-controls="logo-sidebar" type="button" class="inline-flex items-center p-2 mt-2 ms-3 text-sm text-gray-500 rounded-lg lg:hidden focus:outline-none focus:ring-2 focus:ring-gray-200">
   <span class="sr-only">Open sidebar</span>
   <svg class="w-10 h-10" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
   <path clip-rule="evenodd" fill-rule="evenodd" d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z"></path>
   </svg>
 </button>
 
-<aside id="logo-sidebar" class="fixed top-0 left-0 z-40 md:w-72 w-60 h-screen transition-transform -translate-x-full sm:translate-x-0" aria-label="Sidebar">
+<aside id="logo-sidebar" class="fixed top-0 left-0 z-40 md:w-72 w-60 h-screen transition-transform -translate-x-full lg:translate-x-0" aria-label="Sidebar">
   <div class="h-full px-3 py-2 overflow-y-auto bg-unguTipis flex flex-wrap justify-center items-center">
      <ul class="space-y-2 font-medium" id="side-nav">
       {{-- LOGO DASHBOARD --}}
--- resources/views/layouts/dashboard-layout.blade.php
+++ resources/views/layouts/dashboard-layout.blade.php
@@ -13,8 +13,11 @@
   @include('components.nav-dashboard.dashboard_navbar')
   
   {{-- SIDE CONTENT --}}
-  <div class="p-4 sm:ml-64">
-    @yield('content')
+  <div class="p-6 lg:p-12 lg:ml-72 h-screen" id="dashboard-content">
+    <div class="grid grid-rows-3 h-full w-full place-items-center">
+      @yield('content')
+      <h1>tambah dan pagination</h1>
+    </div>
   </div>
 
   <script src="{{ asset('js/dash.js') }}"></script>
