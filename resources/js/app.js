334f921e2195923796c2d7c0484ec19340fa2c26
fathursyh <fathur.syah1@gmail.com>
2024-09-22 22:34:46 +0700

    update : commit after revert

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,11 @@
 import './bootstrap';
+import 'flowbite';
+import AOS from 'aos';
+import 'aos/dist/aos.css'; 
+
+AOS.init({
+  startEvent: 'DOMContentLoaded',
+  duration: 600,
+  offset: 280,
+  easing: 'ease-out-cubic'
+});
---
+++ tailwind.config.js
@@ -0,0 +1,37 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./resources/**/*.blade.php",
+    "./resources/**/*.js",
+    "./resources/**/*.vue",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+module.exports = {
+  content: [
+    "./resources/**/*.blade.php",
+    "./resources/**/*.js",
+    "./resources/**/*.vue",
+    "./node_modules/flowbite/**/*.js"
+  ],
+  theme: {
+    colors: {
+      unguTipis: '#DFE2F2',
+      ungu: '#7A75B4',
+      hijau: '#00A858'
+    },
+    extend: {},
+  },
+  safelist: [
+    '/^datatable-.*$/'
+  ],
+  plugins: [
+      require('flowbite/plugin')({
+        charts: true,
+      })
+  ],
+}
